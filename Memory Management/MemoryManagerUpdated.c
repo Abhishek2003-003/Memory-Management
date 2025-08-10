565c058f3dd8358b6aeb63258f05905eafd43d05
Abhishek2003-003 <abhishek.200364a@gmail.com>
2025-08-10 18:34:42 +0530

    Update MemoryManagerUpdated.c

--- Memory Management/MemoryManagerUpdated.c
+++ Memory Management/MemoryManagerUpdated.c
@@ -115,22 +115,6 @@ void print_state()
     printf("\n");
 }
 
-// Predefined test cases for initial run
-void run_tests() 
-{
-    printf("==== Running Predefined Test Cases ====\n");
-    allocate(0, 128);   // mem[0]
-    allocate(1, 1024);  // mem[1]
-    allocate(2, 4096);  // mem[2]
-    print_state();
-
-    deallocate(1);      // should pack mem[2] down
-    print_state();
-
-    allocate(1, 512);   // allocate at mem[1] again
-    print_state();
-    printf("**** Test Cases Completed *****\n\n");
-}
 
 int main() 
 {
@@ -143,8 +127,7 @@ int main()
         memory[i] = 0;
     }
 
-    // Run automated test cases first
-    run_tests();
+  
 
     while (1) {
         int choice;
